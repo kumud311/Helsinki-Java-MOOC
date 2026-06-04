1781645ba4fcb5d64cb2010784f2e5d89224b2ec
Kumud <kkumud615@gmail.com>
2026-06-04 12:49:22 +0530

    Complete and sync exercises 12 to 14

--- part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
+++ part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
@@ -5,8 +5,10 @@ public class BooleanInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // write your program here
+        System.out.println("Write something:");
+        boolean value = Boolean.parseBoolean(scanner.nextLine());
+        System.out.println("True or false? " + value);
+        
 
     }
 }
