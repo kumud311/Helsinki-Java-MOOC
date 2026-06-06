60a6902c4924cbba4a80a0e87b53589c5ffd685f
Kumud <kkumud615@gmail.com>
2026-06-06 13:34:33 +0530

    Complete and sync exercises 16 1to 20

--- part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
+++ part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
@@ -5,8 +5,11 @@ public class SumOfTwoNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
-
+        System.out.println("Give the first number:");
+        int first = Integer.parseInt(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.parseInt(scanner.nextLine());
+        int sum = first+second;
+        System.out.println( "The sum of the numbers is "+sum);
     }
 }
--- part01-Part01_19.AdditionFormula/src/main/java/AdditionFormula.java
+++ part01-Part01_19.AdditionFormula/src/main/java/AdditionFormula.java
@@ -5,8 +5,12 @@ public class AdditionFormula {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // write your program here
+        System.out.println("Give the first number:");
+        int first = Integer.parseInt(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.parseInt(scanner.nextLine());
+        int sum = first+second;
+        System.out.println(first + " + " + second + " = " + sum);
 
     }
 }
