cdca840fe44ba0b76a623424952f37cf1c2f68fb
Kumud <kkumud615@gmail.com>
2026-06-07 22:51:16 +0530

    ex 21 - TMC not downloading properly

--- part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
+++ part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
@@ -1,12 +1,19 @@
 
-import java.util.Scanner;
+import java.util.scanner;
 
 public class AverageOfThreeNumbers {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new scanner(System.in);
 
-        // Write your program here
-
+        System.out.println("Give the first number: ");
+        int first = Integer.parseInt(scanner.nextLine());
+        System.out.println("Give the second number: ");
+        int second = Integer.parseInt(scanner.nextLine());
+        System.out.println("Give the third number: ");
+        int third = Integer.parseInt(scanner.nextLine());
+        double average = (first + second + third)/2.0;
+        System.out.println("The average is " + average );
+        
     }
 }
